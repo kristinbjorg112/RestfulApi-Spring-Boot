e9b46b45687e3288e63a12fbcb29968e88b678e1
kristinbjorg112 <kristinbjorg112@gmail.com>
2020-03-25 08:42:17 +0000

    k

--- src/main/java/com/example/rest/repo/VesselRepo.java
+++ src/main/java/com/example/rest/repo/VesselRepo.java
@@ -3,7 +3,6 @@ package com.example.rest.repo;
 import com.example.rest.model.VesselModel;
 import org.springframework.data.repository.CrudRepository;
 
-
 public interface VesselRepo extends CrudRepository<VesselModel,Long> {
     }
 
